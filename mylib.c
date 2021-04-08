ead8861c2a9673fbeb39b3c9e9be7ed393365308
Gabriele Barberis Web <alertime1wp@gmail.com>
2021-04-08 10:34:29 +0000

    Initial commit

---
+++ mylib.c
@@ -0,0 +1,21 @@
+#include "mylib.h"
+
+
+//FUNZIONE DI CONTROLLO DIMENSIONE E CARICAMENTO VETTORE
+int insvect(int vect[])
+{
+  
+}
+
+
+//FUNZIONE DI STAMPA VETTORE
+int printvect(int vect[],int dim)
+{
+  
+}
+
+//FUNZIONE DI ORDINAMENTO VETTORE PER OSTITUZIONE
+int ord_sost(int vect[],int dim) 
+{
+// STAMPARE IL VETTORE OGNI PASSAGGIO
+}
